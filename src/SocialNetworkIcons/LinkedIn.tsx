4a717a6f0d0d5ed746a7172c1476e7393616f9e3
ManuchehrTilloev <manuchehr.tilloev@gmail.com>
2023-10-01 15:59:22 +0500

    fix

--- src/SocialNetworkIcons/LinkedIn.tsx
+++ src/SocialNetworkIcons/LinkedIn.tsx
@@ -2,14 +2,8 @@ import React from 'react';
 
 import { IIconProps } from '../types';
 
-const LinkedIn: React.FC<IIconProps> = ({ width = '40', height = '40' }) => (
-  <svg
-    width={width}
-    height={height}
-    viewBox="0 0 41 40"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+const LinkedIn: React.FC<IIconProps> = () => (
+  <svg width="40" height="40" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="Frame 233">
       <rect x="0.16272" width="40" height="40" rx="8" fill="#111111" />
       <path
