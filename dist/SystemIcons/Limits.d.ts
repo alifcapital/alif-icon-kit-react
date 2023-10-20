dd43749bc5eee171de03b1618b945884fb5b9b30
Romesh28 <romesh280301@mail.ru>
2023-10-20 15:59:29 +0500

    added icons passport limits subscription and logout

---
+++ dist/SystemIcons/Limits.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { IIconProps } from '../types';
+declare const Limits: React.FC<IIconProps>;
+export default Limits;
--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,9 @@ import EyeOn from './SystemIcons/EyeOn';
 import Search from './SystemIcons/Search';
 import Bill from './SystemIcons/Bill';
 import Qr from './SystemIcons/Qr';
+import Passport from './SystemIcons/Passport';
+import Limits from './SystemIcons/Limits';
+import LogOut from './SystemIcons/LogOut';
 import Calendar from './SystemIcons/Calendar';
 import Chat from './SystemIcons/Chat';
 import Check from './SystemIcons/Check';
@@ -138,6 +141,9 @@ export {
   Search,
   Bill,
   Qr,
+  Passport,
+  Limits,
+  LogOut,
   Calendar,
   Chat,
   Check,
